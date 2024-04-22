8c34a19ae8de17e5bae2532d0ffe62679a5df15e
Rayasd396kr <rayasd396kr@gmail.com>
2024-04-23 00:39:24 +0800

    N2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,8 +6,8 @@
 //   modules: ["@tresjs/nuxt"],
 // });
 export default defineNuxtConfig({
-    app: {
-      baseURL: process.env.NODE_ENV === 'production' ? '/nuxt3-generate/' : '/',
-      buildAssetsDir: '/static/'
-    }
+  app: {
+    baseURL: '/nuxt-github-pages/', // baseURL: '/<repository>/'
+    buildAssetsDir: 'assets', // don't use "_" at the begining of the folder name to avoids nojkill conflict
+  }
 })
