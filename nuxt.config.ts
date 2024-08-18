a7935a032a1d57bb9797428b3437af7c131b0077
Rayasd396kr <rayasd396kr@gmail.com>
2024-08-18 20:40:53 +0800

    8/18 1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,5 @@
 export default defineNuxtConfig({
   nitro: {
     preset: 'cloudflare' // 預設為 node-server
-<<<<<<< HEAD
   }
-=======
-  },
-
-  compatibilityDate: "2024-07-05"
->>>>>>> 183b0be7288518620af4ae4eac0399207f4a7fc9
 })
